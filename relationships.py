cbf0310b73a95ef7f6723c7ba80bd251e5721799
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2019-08-22 22:55:10 +1200

    Remove unused import

--- relationships.py
+++ relationships.py
@@ -14,7 +14,6 @@ from org.apache.lucene.index import DirectoryReader
 from org.apache.lucene.queryparser.classic import QueryParser
 from org.apache.lucene.search import IndexSearcher
 from org.apache.lucene.store import SimpleFSDirectory
-from org.apache.lucene.util import Version
 
 # create logger
 logger = logging.getLogger('compare_news')
