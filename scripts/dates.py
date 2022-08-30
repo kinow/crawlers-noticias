2c76d5234ee02959638cea3135a26042cd80fad2
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2022-08-31 10:49:15 +1200

    Fix formatting

--- scripts/dates.py
+++ scripts/dates.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python
 
+"""Correct date format for the JN news."""
+
 import sys
 import json
 import datetime
 
 def main():
-	# JN date format is not right
-	with open('jn.json', 'r') as f:
-    data = json.load(f)
-    new_data = []
-    for entry in data:
-      date = datetime.datetime.strptime(entry['date'], '%Y/%m/%d')
-      entry['date'] = str(date)
-      new_data.append(entry)
+    """Main function."""
+	  with open('jn.json', 'r') as f:
+        data = json.load(f)
+        new_data = []
+        for entry in data:
+            date = datetime.datetime.strptime(entry['date'], '%Y/%m/%d')
+            entry['date'] = str(date)
+            new_data.append(entry)
 
-	with open('jn2.json', 'w+') as f:
-	  json.dump(new_data, f)
+  	with open('jn2.json', 'w+') as f:
+	      json.dump(new_data, f)
 
 if __name__ == '__main__':
     main()
