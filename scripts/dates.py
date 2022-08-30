04f8894d8e850805d09be8ed54667e5e37bbbf98
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2022-08-31 10:47:43 +1200

    Use context manager

--- scripts/dates.py
+++ scripts/dates.py
@@ -6,18 +6,16 @@ import datetime
 
 def main():
 	# JN date format is not right
-	f = open('jn.json', 'r')
-	data = json.load(f)
-	new_data = []
-	for entry in data:
-		date = datetime.datetime.strptime(entry['date'], '%Y/%m/%d')
-		entry['date'] = str(date)
-		new_data.append(entry)
-	f.close()
+	with open('jn.json', 'r') as f:
+    data = json.load(f)
+    new_data = []
+    for entry in data:
+      date = datetime.datetime.strptime(entry['date'], '%Y/%m/%d')
+      entry['date'] = str(date)
+      new_data.append(entry)
 
-	f = open('jn2.json', 'w+')
-	json.dump(new_data, f)
-	f.close()
+	with open('jn2.json', 'w+') as f:
+	  json.dump(new_data, f)
 
 if __name__ == '__main__':
     main()
